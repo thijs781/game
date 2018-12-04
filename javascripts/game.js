69a4cfe2f8c25dc0f393abc0198b9d0f72bb77c3
Gino <tramontina@live.nl>
2018-12-04 17:32:45 +0100

    Updated html and implemented javascript
    implemented CanMoveLeft and MoveLeft and some other methods and altered the id's of the table cells in the html code to be more easy to use in jQuery.
    Need to split server code and game code into 2 files( not yet done)

--- javascripts/game.js
+++ javascripts/game.js
@@ -8,8 +8,9 @@ const bodyParser = require("body-parser");
 
 var users = 0; //counts the amount of user on the site
 var activegames = 0; //keeps track of the amount of games being played
-
-
+var PiecesLost1 = 0; //keeps track of the number of pieces Player 1 lost
+var PiecesLost2 = 0; //keeps track of the number of pieces Player 2 lost
+var temp = 0; //DELETE WHEN DONE!!! 
 
 // give the player a cookie with an userId
 app.use(cookies("secur")); // this will encrypt cookies to avoid users tampering with them
@@ -27,7 +28,6 @@ app.use(function(req, res, next) {
 
 //update board
 //updates the board on the website
-//shows player 2 a rotated version
 /////////////////////////////////////////////////////////////////////////
 
 //Turn
@@ -36,11 +36,52 @@ app.use(function(req, res, next) {
 /////////////////////////////////////////////////////////////////////////
 
 
+//pieceLost
+//increments the counter of lost pieces of a player
+function pieceLost(player){
+	if(player == 1)PiecesLost1++; //if the player is Player1 increments the counter of lost pieces of Player1
+	if(player == 2)PiecesLost2++; //if the player is Player2 increments the counter of lost pieces of Player2
+}
+
+//oponentof
+//returns te playerid of the opponent of player
+function opponentof(player){
+	if(player==1) return 2;
+	if(player==2) return 1;
+}
+
+//clearSquare
+function clearSquare(x,y){
+	$("#row"+x+"col"+y).html() = null;
+}
 //CanLeft
-//up 1, left 1 == open
-//if false: up 2, left 2 == open
+function CanLeft(x,y,player){
+	var newx = x - 1;
+	if(newx<0) return;
+	var newy = y - 1;
+	if(newy<0)return;
+	if(content(newx,newy) == null) $("#row"+newx+"col"+newy).html("insert button here"); //make a button that let's the player do: MoveLeft(newx,newy,false,player)
+	if(content(newx,newy) == pieceof(player)) return;
+	else { var tempo = 0; //check if can take
+		newx--;
+		if(newx<0) return;
+		newy--;
+		if(newy<0) return;
+		if(content(newx,newy) == pieceof(opponentof(player))) $("#row"+newx+"col"+newy).html("insert button here");//make a button that let's the player do: MoveLeft(newx,newy,false,player)
+	}
+}
+
 
 //MoveLeft
+function MoveLeft(x,y,cont,take, player){
+	content(x,y) = cont;
+	if(take == true){
+		clearSquare(newx+1,newy+1);
+		pieceLost(oponentof(player));
+	}
+		
+
+}
 //if open: up 1, left 1
 //if them: if open: up 2, left 2; then check if can hit again
 /////////////////////////////////////////////////////////////////////////
@@ -81,6 +122,3 @@ http.createServer(app).listen(3000, function(){
 
 
 
-
-
-$("#rij1col1").html()

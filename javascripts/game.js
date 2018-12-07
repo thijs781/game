d1450845b425a89a2fb885131aad20e9a220b555
thijs781 <thijs.engelvaart@gmail.com>
2018-12-07 10:34:13 +0100

    all most finished game
    a few more thing needed

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,25 +1,11 @@
 
 
 
-<<<<<<< Updated upstream
-var users = 0; //counts the amount of user on the site
-var activegames = 0; //keeps track of the amount of games being played
-var PiecesLost1 = 0; //keeps track of the number of pieces Player 1 lost
-var PiecesLost2 = 0; //keeps track of the number of pieces Player 2 lost
-var temp = 0; //DELETE WHEN DONE!!! 
-=======
 
-
+var TurnOf = 0; //keeps track of whos turn it is
 var PiecesLost1 = 0; //keeps track of the number of pieces Player 1 lost
 var PiecesLost2 = 0; //keeps track of the number of pieces Player 2 lost
 
->>>>>>> Stashed changes
-
-
-
-//update board
-//updates the board on the website
-/////////////////////////////////////////////////////////////////////////
 
 //Turn
 //shows whos turn it is
@@ -30,9 +16,16 @@ var PiecesLost2 = 0; //keeps track of the number of pieces Player 2 lost
 //pieceLost
 //increments the counter of lost pieces of a player
 function pieceLost(player){
-	if(player == 1)PiecesLost1++; //if the player is Player1 increments the counter of lost pieces of Player1
-	if(player == 2)PiecesLost2++; //if the player is Player2 increments the counter of lost pieces of Player2
+	if(player == 1){
+		PiecesLost1++; //if the player is Player1 increments the counter of lost pieces of Player1
+		$("#P1_lost").html(PiecesLost1)
+	}
+	if(player == 2){
+		PiecesLost2++; //if the player is Player2 increments the counter of lost pieces of Player2
+		$("#P2_lost").html(PiecesLost2)
+	}
 }
+
 
 //oponentof
 //returns te playerid of the opponent of player
@@ -43,81 +36,177 @@ function opponentof(player){
 
 //clearSquare
 function clearSquare(x,y){
-<<<<<<< Updated upstream
-	$("#row"+x+"col"+y).html() = null;
-=======
-	$("#row"+x+"col"+y).empty();
->>>>>>> Stashed changes
+	$("#row"+y+"col"+x).empty();
 }
+
 //CanLeft
 function CanLeft(x,y,player){
+	$("button").remove(".mov");
 	var newx = x - 1;
-	if(newx<0) return;
-	var newy = y - 1;
-	if(newy<0)return;
-<<<<<<< Updated upstream
-	if(content(newx,newy) == null) $("#row"+newx+"col"+newy).html("insert button here"); //make a button that let's the player do: MoveLeft(newx,newy,false,player)
-=======
-	if(content(newx,newy) == "") $("#row"+newx+"col"+newy).html("insert button here"); //make a button that let's the player do: MoveLeft(newx,newy,false,player)
->>>>>>> Stashed changes
-	if(content(newx,newy) == pieceof(player)) return;
-	else { var tempo = 0; //check if can take
-		newx--;
-		if(newx<0) return;
-		newy--;
-		if(newy<0) return;
-		if(content(newx,newy) == pieceof(opponentof(player))) $("#row"+newx+"col"+newy).html("insert button here");//make a button that let's the player do: MoveLeft(newx,newy,false,player)
+	if(newx>=0){
+		var newy = y - 1;
+		if(newy>=0){
+			if($("#row"+newy+"col"+newx).html() == "") {
+				$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveLeft("+x+","+y+","+newx+","+newy+",false,"+player+")' class='mov'></button>");//make a button that let's the player do: MoveLeft(newx,newy,false,player)
+			}
+
+			if($("#row"+newy+"col"+newx).html().includes('class="P'+opponentof(player)+'"')){//check if can take
+				newx--;
+				if(newx>=0){
+					newy--;
+					if(newy>=0){
+						if($("#row"+newy+"col"+newx).html() == "") { 
+							$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveLeft("+x+","+y+","+newx+","+newy+",true,"+player+")' class='mov'></button>"); //make a button that let's the player do: MoveLeft(newx,newy,true,player)
+							cantake = 1;
+						}
+					}
+				}
+			}
+		}
 	}
 }
 
 
 //MoveLeft
-function MoveLeft(x,y,cont,take, player){
-	content(x,y) = cont;
+function MoveLeft(x,y,newx,newy,take,player){
+	//$("#movbut").remove();
+	$("#row"+newy+"col"+newx).html('<button id="2" align="center" onclick="CanLeft('+newx+','+newy+','+player+'),CanRight('+newx+','+newy+','+player+')" class="P'+player+'"></button>');
+	$("#row"+y+"col"+x).empty();
 	if(take == true){
 		clearSquare(newx+1,newy+1);
-		pieceLost(oponentof(player));
+		pieceLost(opponentof(player));
 	}
-		
-
+	$("#movbut").remove();
 }
-//if open: up 1, left 1
-//if them: if open: up 2, left 2; then check if can hit again
-/////////////////////////////////////////////////////////////////////////
+
 
 //CanRight
-//up 1, right 1 == open
-//if false: up 2, right 2 == open
+function CanRight(x,y,player){
+	var newx = x + 1;
+	if(newx<8){
+		var newy = y - 1;
+		if(newy>=0){
+			if($("#row"+newy+"col"+newx).html() == "") {
+				$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveRight("+x+","+y+","+newx+","+newy+",false,"+player+")' class='mov'></button>");//make a button that let's the player do: MoveLeft(newx,newy,false,player)
+			} 
+			//if($("#row"+newy+"col"+newx) == pieceof(player)) return;
+			if($("#row"+newy+"col"+newx).html().includes('class="P'+opponentof(player)+'"')) {  //check if can take
+				newx++;
+				if(newx<8){
+					newy--;
+					if(newy>=0){
+						if($("#row"+newy+"col"+newx).html() == "") { 
+							$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveRight("+x+","+y+","+newx+","+newy+",true,"+player+")' class='mov'></button>"); //make a button that let's the player do: MoveLeft(newx,newy,true,player)
+							cantake = 1;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 
 //MoveRight
-//if open: up 1, right 1
-//if them: if open: up 2, right 2; then check if can hit again
-/////////////////////////////////////////////////////////////////////////
-
-//CanLeftBack
-//down 1, left 1 == open
-//if false: down 2, left 2 == open
-
-//MoveLeftBack
-//if open: down 1, left 1
-//if them: if open: down 2, left 2; then check if can hit again
-/////////////////////////////////////////////////////////////////////////
-
-//CanRightBack
-//down 1, right 1 == open
-//if false: down 2, right 2 == open
-
-//MoveRightBack
-//if open: down 1, right 1
-//if them: if open: down 2, right 2; then check if can hit again
-/////////////////////////////////////////////////////////////////////////
+function MoveRight(x,y,newx,newy,take,player){
+	//$("#movbut").remove();
+	$("#row"+newy+"col"+newx).html('<button id="2" align="center" onclick="CanLeft('+newx+','+newy+','+player+'),CanRight('+newx+','+newy+','+player+')" class="P'+player+'"></button>');
+	$("#row"+y+"col"+x).empty();
+	if(take == true){
+		clearSquare(newx-1,newy+1);
+		pieceLost(opponentof(player));
+	}
+	$("#movbut").remove();
+}
 
 
 
 
 
-<<<<<<< Updated upstream
-=======
+
+//CanBackLeft
+function CanBackLeft(x,y,player){
+	$("button").remove(".mov");
+	var newx = x - 1;
+	if(newx>=0){
+		var newy = y + 1;
+		if(newy>=0){
+			if($("#row"+newy+"col"+newx).html() == "") {
+				$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveBackLeft("+x+","+y+","+newx+","+newy+",false,"+player+")' class='mov'></button>");//make a button that let's the player do: MoveLeft(newx,newy,false,player)
+			}
+
+			if($("#row"+newy+"col"+newx).html().includes('class="P'+opponentof(player)+'"')){//check if can take
+				newx--;
+				if(newx>=0){
+					newy++;
+					if(newy>=0){
+						if($("#row"+newy+"col"+newx).html() == "") { 
+							$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveBackLeft("+x+","+y+","+newx+","+newy+",true,"+player+")' class='mov'></button>"); //make a button that let's the player do: MoveLeft(newx,newy,true,player)
+							cantake = 1;
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 
->>>>>>> Stashed changes
+//MoveBackLeft
+function MoveBackLeft(x,y,newx,newy,take,player){
+	//$("#movbut").remove();
+	$("#row"+newy+"col"+newx).html('<button id="2" align="center" onclick="CanBackLeft('+newx+','+newy+','+player+'),CanBackRight('+newx+','+newy+','+player+')" class="P'+player+'"></button>');
+	$("#row"+y+"col"+x).empty();
+	if(take == true){
+		clearSquare(newx+1,newy+1);
+		pieceLost(opponentof(player));
+	}
+	$("#movbut").remove();
+}
+
+
+//CanBackRight
+function CanBackRight(x,y,player){
+	var newx = x + 1;
+	if(newx<8){
+		var newy = y + 1;
+		if(newy>=0){
+			if($("#row"+newy+"col"+newx).html() == "") {
+				$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveBackRight("+x+","+y+","+newx+","+newy+",false,"+player+")' class='mov'></button>");//make a button that let's the player do: MoveLeft(newx,newy,false,player)
+			} 
+			//if($("#row"+newy+"col"+newx) == pieceof(player)) return;
+			if($("#row"+newy+"col"+newx).html().includes('class="P'+opponentof(player)+'"')) {  //check if can take
+				newx++;
+				if(newx<8){
+					newy++;
+					if(newy>=0){
+						if($("#row"+newy+"col"+newx).html() == "") { 
+							$("#row"+newy+"col"+newx).html("<button id='movbut' align='center' onclick='MoveBackRight("+x+","+y+","+newx+","+newy+",true,"+player+")' class='mov'></button>"); //make a button that let's the player do: MoveLeft(newx,newy,true,player)
+							cantake = 1;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+//MoveRight
+function MoveBackRight(x,y,newx,newy,take,player){
+	//$("#movbut").remove();
+	$("#row"+newy+"col"+newx).html('<button id="2" align="center" onclick="CanBackLeft('+newx+','+newy+','+player+'),CanBackRight('+newx+','+newy+','+player+')" class="P'+player+'"></button>');
+	$("#row"+y+"col"+x).empty();
+	if(take == true){
+		clearSquare(newx-1,newy-1);
+		pieceLost(opponentof(player));
+	}
+	$("#movbut").remove();
+}
+
+
+
+
+
+
+
